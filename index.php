1d2f5a4cf66b5f8831e2dac0e9af07c889339f0c
Richard Omollo <Richard Omollo>
2016-08-11 22:00:17 +0200

    initial commit skeleton sass starter template

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Starter Template for Sass </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="HandheldFriendly" content="True">
+        <meta name="MobileOptimized" content="320">
+
+       <!--  Include html5shim by Sjoerd Visscherto enable styling of HTML5 elements in versions of Internet Explorer prior to version 9 -->
+
+        <!--[if lt IE 9]>
+            <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+        <!--[if IE 7]>     <html class="ie7"> <![endif]-->  
+        <!--[if IE 8]>     <html class="ie8"> <![endif]-->  
+        <!--[if IE 9]>     <html class="ie9"> <![endif]-->  
+        <!--[if !IE]><!--> <html>             <!--<![endif]-->
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+        <h1>hello world!</h1>
+    </body>
+</html>
